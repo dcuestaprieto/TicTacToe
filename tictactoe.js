37f9da635afcf445cbc46d8189fc438fd9906b41
dcuestaprieto <93988102+dcuestaprieto@users.noreply.github.com>
2023-03-14 09:48:15 +0100

    TicTacToe Final

--- tictactoe.js
+++ tictactoe.js
@@ -69,12 +69,33 @@ let combinacionesGanadoras = [
  * -------------------------------------------------------------------------
  */
 let tocaJugar=true;
+let combinacionesJuegoO=[];
+let combinacionesJuegoX=[];
+let contadorX, contadorO;
+let contadorVictoriaX=0, contadorVictoriaO=0;
+let button, pVictoriasX, pVictoriasO;
+let parrafoVictorias;
+
+parrafoVictorias=document.getElementById('p-resultados');
+
+parrafoVictorias.textContent=`O: ${contadorVictoriaO} e X: ${contadorVictoriaX}`;
+
+button=document.createElement('button');
+button.setAttribute('onclick','resetear()');
+button.textContent='Resetear';
+
+document.getElementsByClassName('tablero')[0].appendChild(button);
+
 function agregarFicha(numero){
-    let casilla=document.getElementsByTagName('div')[numero+1];
+    //Esta funcion se usa para pintar X u O según se va cambiando el turno
+    let casillaFun=document.getElementsByClassName("casilla")[numero];
+    //Se llama al método en funcion de a quién le toca jugar y ahí cambian los parámetros
     if(tocaJugar){
-        casilla.textContent="O";
+        casillaFun.textContent="O";
+        agregarPosicion(combinacionesJuegoO,numero, contadorO);
     }else{
-        casilla.textContent="X";
+        casillaFun.textContent="X";
+        agregarPosicion(combinacionesJuegoX,numero,contadorX);
     }
     cambiarValor();
     /**
@@ -85,11 +106,67 @@ function agregarFicha(numero){
 }
 
 function cambiarValor(){
+    //Cambiar el valor después de que se haya jugado
     if(tocaJugar){
         tocaJugar=false;
     }else{
         tocaJugar=true;
     }
+}
+
+function agregarPosicion(arr, numero, combinaciones){
+    // console.log(numero);
+    //Al array (dependiendo de si es x u o) se añade el número que ha desembocado la llamada onclick
+    arr.push(numero);
+    // console.log(arr);
+    for(combinacion in combinacionesGanadoras){
+        // console.log(combinacionesGanadoras[combinacion]);
+        for(i in arr){
+            // console.log(arr[i]);
+            //Hay que recorrer el array de combinaciones ganadoras y el array de X u O
+            if(combinacionesGanadoras[combinacion].includes(arr[i])){
+                //Si coincide alguno de los valores del array de X u O pues se suma un contador que debe ir hasta 3 y eso es que coincidan todos
+                // console.log(arr[i]+" es una combinacion ganadora");
+                combinaciones++;
+            }
+        }
+        if(combinaciones==3){
+            //Si es igual a 3 es que hay una combinacion ganadora por lo que hay que saltar victoria
+            if(tocaJugar){
+                console.log("Victoria O");//Mostrar de quien es la victoria
+                contadorVictoriaO++;//Sumar uno a la victoria de X u O
+                parrafoVictorias.textContent=`O: ${contadorVictoriaO} e X: ${contadorVictoriaX}`;//Actualizar la tabla de resultados
+                quitarOnclick();//Quitar los eventos click de las demás casillas
+
+            }else{
+                //En el else es lo mismo que el anterior pero con las X
+                console.log("Victoria X");
+                contadorVictoriaX++;
+                parrafoVictorias.textContent=`O: ${contadorVictoriaO} e X: ${contadorVictoriaX}`;
+                quitarOnclick();
+            }
+        }
+        combinaciones=0;
+    }
+}
+
+function quitarOnclick(){
+    //Esta funcion quita los onclick de todas las casillas recorriendolas todas
+    for (let i = 0; i < casillas.length; i++) {
+        casillas[i].removeAttribute('onclick');
+    }
+}
+
+function resetear(){
+    for (let i = 0; i < casillas.length; i++) {
+        casillas[i].textContent='';//Pone a vacio el textContent
+        casillas[i].setAttribute('onclick',`agregarFicha(${i})`);//Agrega el onclick para el método agregarFicha
+    }
+    //Resetea todos los arrays de X y O
+    combinacionesJuegoO=[];
+    combinacionesJuegoX=[];
+    //Refresco el contador
+    console.log("O: ",contadorVictoriaO," e X:",contadorVictoriaX);
 }
 
 /**
